050860ad4ebba336c94c84aa5f7be4a7ead300ec
Barry Day <brday7@gmail.com>
2024-04-01 18:36:06 -0700

    Intergalactic Burger War progress
    Cleaning up pics in Illustrator

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// Page Transition
+// https://www.youtube.com/watch?v=ckJ7gdIeebc
+
+// window.onload = () =>
+// {
+//     const transition_element = document.querySelector('.transition');
+//     const anchors = document.querySelectorAll('a');
+
+//     setTimeout(() => 
+//     {
+//         transition_element.classList.remove('is-active');
+//     }, 500)
+
+
+//     for (let i = 0; i < anchors.length; ++i)
+//     {
+//         const anchor = anchors[i];
+//         anchor.addEventListener('click', e =>
+//         {
+//             e.preventDefault();
+//             let target = e.target.href;
+
+//             transition_element.classList.add('is-active');
+
+//             setTimeout(() => 
+//             {
+//                 window.location.href = target;
+//             }, 500);
+//         })
+//     }
+// }
+
+
 // Portfolio Dropdown
 // container is the outer-most element, holds dropdown
 // dropdown holds the portfolio links and is the thing that will be shown/hidden
@@ -39,9 +72,3 @@ function closePortfolioDropdownOnOutsideClick(event)
         closePortfolioDropdown();
     }
 }
-
-
-
-// Page Transition
-
-// make opacity, transform 
